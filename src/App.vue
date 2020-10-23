f6e8dac61f37a0cca635e1addbd34f7ee73c5980
vikrant <vikrantsingh47@gmail.com>
2020-10-23 17:00:34 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="app-container">
+{{user.username}}
+ </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '@_vikrant',
+        firstname: 'vikrant',
+        lastname: 'singh',
+        email: 'vikrantsingh47@gmail.com',
+        isAdmin: true
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
